928d05ebfb6a7b1e27b2284328d5a1adaafea79e
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-11-19 13:39:55 +0200

    1st exe

---
+++ exDay1.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 10:46:26 2019
+
+@author: owner
+"""
+#Exercieses for day 1
+print ("Ex1")
+print("Hello")
+print("\t Hello")
+print("\t \t Hello")
+
+
+print ("Ex2:")
+print ("Orange Academy\n"*20)
+    
+
+print ("Ex3:")
+print (1.0)
+print (2.8)
+print (3.0)
+print(4.2)
+print('1.0102.8103.0104.2')
+
+
+print ("Ex4:")
+x= int(input('num1'))
+y= int(input('num2'))
+print('product', x*y)
+
+
+print ("Ex5:")
+sum=0
+for a in range(5):
+     x= int(input('num'))
+     sum = sum + x
+print('avg', sum/5)
+
+
+print ("Ex6:")
+print('int')
+print('float')
+print('complex')
+print('str')
+print('bool')
+
+
+print ("Ex7:")
+print("Orange")
+print(1)
+print(100)
+print(-10)
+
+print ("Ex8:")
+x5=10
+print(x5)
+X_q='orange'
+print(X_q)
+AB= 14
+print(AB)
+print(10*10)
+
+
+print ("Ex9:")
+print("Orange")
+print(1)
+print(100)
+print(-10)
+
+
+
+print ("Ex10:")
+x5=10
+print(x5)
+X_q='orange'
+print(X_q)
+AB= 14
+print(AB)
+print(10*10)
+
+
+
+print ("Ex11:")
+print("cheers,Mate!")
+
+
+print ("Ex12:")
+name= (input('name'))
+age= int(input('age'))
+BY = 2019- age
+print('the year when you are 100', BY+100)
+
+
+print ("Ex13:")
+base= int(input('base'))
+height= int(input('height'))
+Area= 0.5*base*height
+print(Area)
+
+print ("Ex14:")
+print ("x+y:", 14)
+print ("x-y:", 8)
+print ("x*y:", 33)
+print ("x/y:", 3.6666666666666665)
+print ("x//y:", 3)
+print ("x%y:",2)
+print ("abs(x*-1):", 11)
+print ("int x:", 11)
+print ("float x:", 11.0)
+print ("divmod(x,y)", 3 , 2)
+print ("pow(11,3)", 1331)
+print ("x**y" , 1331)
+print ("x>y:", "true")
+print ("x==y", "false")
+print ("x!=y", "true")
+
+
+
+
